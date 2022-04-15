ba789c9f4712f8b8c62a4b81bb00861b9a03243a
gottmi1 <91591041+gottmi1@users.noreply.github.com>
2022-04-16 08:09:09 +0900

    Meals 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Fragment } from "react";
 import Header from "./componenets/Layout/Header";
+import Meals from "./componenets/Meals/Meals";
 
 function App() {
   return (
     <Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 }
